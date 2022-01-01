0ccd145093699d25cb34bc335a54e0a409f23a63
peterrabityu <peterrabityu@gmail.com>
2022-01-01 15:41:35 +0800

    chore: add tailwindcss support

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = function (webpackEnv) {
                 },
                 {
                     test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
+                    use: ['style-loader', 'css-loader', 'postcss-loader'],
                 },
             ],
         },
